b78ab5fa8792e8bb1a4a33cd979645bf65ea68aa
Choi_Jinhong <chlwlsghd58@gmail.com>
2020-05-25 09:01:09 +0900

    DAY_11

---
+++ src/main/java/daily/b_2750.java
@@ -0,0 +1,19 @@
+package daily;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class b_2750 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] array = new int[n];
+        for(int i = 0; i < n; i++)
+            array[i] = Integer.parseInt(br.readLine());
+        Arrays.sort(array);
+        for(int i = 0; i < n; i++)
+            System.out.println(array[i]);
+    }
+}
